db6b8a4ce061363f95113a76640aa654dff3eb00
russelltao <russelltao@foxmail.com>
2019-05-13 19:03:55 +0800

    add python

---
+++ python示例代码/404withcache.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import socket
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_address = ("127.0.0.1",12345)
+sock.bind(server_address)
+sock.listen(100)
+
+while True:
+    conn,client_address = sock.accept()
+    try:
+        data = conn.recv(4096)
+        response = 'HTTP/1.1 404 Not Found\r\nCache-Control: max-age=60\r\nContent-Length: 10\r\n\r\nHelloWorld'
+        conn.send(response.encode())
+    finally:
+        conn.close()
